91a85563eec42ef060f38ebd1e8d90f56a583928
Lestlin Robins <lestlinr@gmail.com>
2024-09-08 11:33:10 +0530

    Added dev info section

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,18 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
+import { Info } from "@mui/icons-material";
+
 
 function HomePage() {
+    const [devInfo, showDevInfo] = useState(false)
     return (
         <div>
             <header className="home-page-header">
                 <div className="home-page-header-app-name">
                     Wash.io
                 </div>
-                <div className="home-page-header-info-button">
-                    Info
+                <div className="home-page-header-info-button" onClick={() => showDevInfo(true)}>
+                    <Info></Info>
                 </div>
             </header>
+            {devInfo && (
+                <div className="dev-info-main-section">
+                    <div className="dev-info-section">
+                        <div style={{ fontFamily: 'Jetbrains-B', fontSize: '2.5vh' }}>Dev Information</div>
+                        <br></br>
+                        <div className="first-dev-info">
+                            Developed by Lestlin Robins in React
+                            <br></br>
+                            Instagram:
+                            <a href="https://www.instagram.com/lestlin_robins" target="_blank"> @lestlin_robins</a>
+                            <br></br>
+                            GitHub:
+                            <a href="https://github.com/LestlinRobins" target="_blank"> github.com/lestlinrobins</a>
+                            <br></br>
+                            Website:
+                            <a href="https://lestlinrobins.netlify.app/" target="_blank"> Visit here</a>
+                            <br></br>
+                            Support:
+                            <a href="https://buymeacoffee.com/lestlinrobins" target="_blank"> buymeacoffee.com</a>
+                        </div>
+                        <div className="second-dev-info">
+                            <br></br>
+                            UI/UX: Basil
+                            <br></br>
+                            Instagram:
+                            <a href="https://www.instagram.com/basi__gar/" target="_blank"> @basi__gar</a>
+                            <br></br>
+                            GitHub:
+                            <a href="https://github.com/Basil-World" target="_blank"> github.com/Basil-World</a>
+                            <br></br>
+                        </div>
+                        <div className="second-dev-info">
+                            <br></br>
+                            Native Dev: Sam Joe
+                            <br></br>
+                            Instagram:
+                            <a href="https://www.instagram.com/samjoe.png/" target="_blank"> @samjoe.png</a>
+                            <br></br>
+                            GitHub:
+                            <a href="https://github.com/KingRain" target="_blank"> github.com/KingRain</a>
+                            <br></br>
+                        </div>
+                        <br></br>
+                        <div style={{ fontFamily: 'Jetbrains-B', color: 'grey' }} onClick={() => showDevInfo(false)}>Close</div>
+                    </div>
+                </div>
+            )}
             <div className="home-page-screen">
                 <div className="washing-machine-logo">
                     <img className="washing-machine-img" src="src/assets/main-icon.png" />
